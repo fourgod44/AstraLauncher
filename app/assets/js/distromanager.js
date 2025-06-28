419e5b8192f035e7bee62e3f76a258979686fa79
fourgod44 <81899806+fourgod44@users.noreply.github.com>
2025-06-28 18:12:49 +0900

    endgame

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://210.204.157.130/workspace/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://astrapixelmon.kro.kr/workspace/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
